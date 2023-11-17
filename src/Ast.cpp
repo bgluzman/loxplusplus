01a070dbe16edbe51c307868e93b200a8969b6fe
Brian Gluzman <bgluzman@gmail.com>
2023-11-17 17:33:00 -0500

    print AST as sexp

--- src/Ast.cpp
+++ src/Ast.cpp
@@ -2,15 +2,30 @@
 
 namespace loxpp {
 
+namespace {
+
+void parenthesize(std::ostream& os, std::string_view name,
+                  const std::initializer_list<AstNode *>& exprs) {
+  os << '(' << name;
+  for (const auto& expr : exprs) {
+    if (expr) {
+      os << ' ';
+      os << *expr;
+    }
+  }
+  os << ')';
+}
+
+}  // namespace
+
 std::ostream& operator<<(std::ostream& os, const AstNode& node) {
   std::visit(overloaded{[&os](const Binary& binary) {
-                          if (binary.left)
-                            os << *binary.left;
-                          os << binary.op.lexeme;
-                          if (binary.right)
-                            os << *binary.right;
+                          parenthesize(os, binary.op.lexeme,
+                                       {binary.left.get(), binary.right.get()});
                         },
-                        [&os](const Literal& literal) { os << literal; }},
+                        [&os](const Literal& literal) {
+                          os << "(lit " << literal << ')';
+                        }},
              node);
   return os;
 }
