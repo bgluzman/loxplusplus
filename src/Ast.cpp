36eb018e8d6b92110aa6a2fdd5435e5cca2b199e
Brian Gluzman <bgluzman@gmail.com>
2023-11-20 15:36:10 -0500

    add basic stmts to Ast

--- src/Ast.cpp
+++ src/Ast.cpp
@@ -4,8 +4,8 @@ namespace loxpp {
 
 namespace {
 
-void parenthesize(std::ostream& os, std::string_view name,
-                  const std::initializer_list<Expr *>& exprs) {
+std::ostream& parenthesize(std::ostream& os, std::string_view name,
+                           const std::initializer_list<Expr *>& exprs) {
   os << '(' << name;
   for (const auto& expr : exprs) {
     if (expr) {
@@ -13,19 +13,56 @@ void parenthesize(std::ostream& os, std::string_view name,
       os << *expr;
     }
   }
-  os << ')';
+  return os << ')';
 }
 
 }  // namespace
 
 std::ostream& operator<<(std::ostream& os, const Binary& binary) {
-  parenthesize(os, binary.op.lexeme, {binary.left.get(), binary.right.get()});
-  return os;
+  return parenthesize(os, binary.op.lexeme,
+                      {binary.left.get(), binary.right.get()});
 }
 
 std::ostream& operator<<(std::ostream& os, const Expr& expr) {
   return std::visit([&os](auto&& expr) -> std::ostream& { return os << expr; },
                     expr.value);
+}
+
+std::ostream& operator<<(std::ostream& os, const Expression& expression) {
+  return os << expression.expr;
+}
+
+std::ostream& operator<<(std::ostream& os, const Block& block) {
+  os << "(block";
+  for (const auto& stmt : block.stmts)
+    os << ' ' << *stmt;
+  return os << ')';
+}
+
+std::ostream& operator<<(std::ostream& os, const Function& function) {
+  os << "(function " << function.name.lexeme << ' ';
+  {
+    os << "(params";
+    for (const auto& param : function.params)
+      os << ' ' << param.lexeme;
+    os << ") ";
+  }
+  {
+    os << "(body";
+    for (const auto& stmt : function.body)
+      os << ' ' << *stmt;
+    os << ')';
+  }
+  return os << ')';
+}
+
+std::ostream& operator<<(std::ostream& os, const Return& return_) {
+  return parenthesize(os, "return", {return_.value.get()});
+}
+
+std::ostream& operator<<(std::ostream& os, const Stmt& stmt) {
+  return std::visit([&os](auto&& stmt) -> std::ostream& { return os << stmt; },
+                    stmt.value);
 }
 
 }  // namespace loxpp
--- src/Ast.h
+++ src/Ast.h
@@ -6,10 +6,12 @@
 #include <memory>
 #include <ostream>
 #include <variant>
+#include <vector>
 
 namespace loxpp {
 
 struct Expr;
+struct Stmt;
 
 struct Binary {
   std::unique_ptr<Expr> left;
@@ -23,5 +25,34 @@ struct Expr {
   Value value;
 };
 std::ostream& operator<<(std::ostream& os, const Expr& expr);
+
+struct Expression {
+  std::unique_ptr<Expr> expr;
+};
+std::ostream& operator<<(std::ostream& os, const Expression& expression);
+
+struct Block {
+  std::vector<std::unique_ptr<Stmt>> stmts;
+};
+std::ostream& operator<<(std::ostream& os, const Block& block);
+
+struct Function {
+  Token                              name;
+  std::vector<Token>                 params;
+  std::vector<std::unique_ptr<Stmt>> body;
+};
+std::ostream& operator<<(std::ostream& os, const Function& function);
+
+struct Return {
+  Token                 keyword;
+  std::unique_ptr<Expr> value;
+};
+std::ostream& operator<<(std::ostream& os, const Return& return_);
+
+struct Stmt {
+  using Value = std::variant<Expression, Block, Function, Return>;
+  Value value;
+};
+std::ostream& operator<<(std::ostream& os, const Stmt& stmt);
 
 }  // namespace loxpp
