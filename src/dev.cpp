c8fd2515c132e7bec7b71a217c408a3ed0f1860e
Brian Gluzman <bgluzman@gmail.com>
2023-11-27 19:50:37 -0500

    separate dev/main binaries for testing

--- src/dev.cpp
+++ src/dev.cpp
@@ -4,23 +4,25 @@
 #include "Token.h"
 
 #include <iostream>
-#include <sstream>
-#include <string>
 
 using namespace loxpp;
 
 int main(int /*argc*/, char * /*argv*/[]) {
-  std::stringstream ss;
-  for (std::string line; std::getline(std::cin, line);) {
-    ss << line << '\n';
-  }
+  std::string source = R"(
+fun test(x) { return (1+2+x)*(x+(1+2)); }
+return test(42);
+)";
 
-  std::string source = ss.str();
-  auto        scanner = Scanner::construct(source);
+  auto scanner = Scanner::construct(source);
   if (!scanner) {
     scanner.error().report();
     return -1;
   }
+  while (!scanner->isAtEnd()) {
+    std::cout << scanner->advance() << '\n' << std::flush;
+  }
+
+  scanner->reset();
 
   Parser parser(std::move(*scanner));
   auto   parse_result = parser.parse();
@@ -28,6 +30,7 @@ int main(int /*argc*/, char * /*argv*/[]) {
     parse_result.error().report();
     return -1;
   }
+  std::cout << *parse_result << '\n' << std::flush;
 
   CodeGenerator codeGen;
   auto          codegen_result = codeGen.generate(*parse_result);
